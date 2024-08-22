df4cd31259f7c6a04d7217a3af9697816ab9fded
Cimar Rodrigo Morales <64667912+CimarRodrigo@users.noreply.github.com>
2024-08-22 15:57:39 -0400

    Delete src directory

--- src/main/java/com/sra/dao/ambienteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sra.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sra.model.ambienteModel;
-
-public interface ambienteDAO extends JpaRepository<ambienteModel,Long> {
-
-}
--- src/main/java/com/sra/dao/requerimientoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sra.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sra.model.requerimientoModel;
-
-public interface requerimientoDAO extends JpaRepository<requerimientoModel,Long>{
-
-}
